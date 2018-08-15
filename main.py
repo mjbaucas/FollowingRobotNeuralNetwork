6b13dd36a20c903082dadfe52f6397eebc00d076
MBaucas <mjbaucas@gmail.com>
2018-08-15 15:37:57 -0400

    Added filter for sensor values greater than 1000

--- main.py
+++ main.py
@@ -17,15 +17,16 @@ if __name__ == "__main__":
     for line in datafile:
         temp_list = line.split(',')
         if len(temp_list) == 8:
-            temp_dict[counter] = [int(temp_list[0]), int(temp_list[1]), int(temp_list[2]), int(temp_list[3]), int(temp_list[4]), int(temp_list[5]), int(temp_list[6]), int(temp_list[7][:-1])]
-            
-            if counter < 7:
-                sensor0_list.append(int(temp_list[0]))
-                sensor1_list.append(int(temp_list[1]))
-                sensor2_list.append(int(temp_list[2]))
-                
-            counter += 1         
-    
+            temp_values = [int(temp_list[0]), int(temp_list[1]), int(temp_list[2]), int(temp_list[3]), int(temp_list[4]), int(temp_list[5]), int(temp_list[6]), int(temp_list[7][:-1])]
+            if all(i <= 1000 for i in temp_values):
+                temp_dict[counter] = temp_values 
+                if counter < 7:
+                    sensor0_list.append(int(temp_list[0]))
+                    sensor1_list.append(int(temp_list[1]))
+                    sensor2_list.append(int(temp_list[2]))
+                    
+                counter += 1         
+
     environment.init(sensor0_list, sensor1_list, sensor2_list)
     mapping_control.init()
     mapping_control.train_mapping(temp_dict, environment)
