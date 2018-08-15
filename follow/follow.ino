e9658ac15508b29fbb7b7e850e17fec8d66e501e
Evan Fallis <efallis@mail.uoguelph.ca>
2018-08-15 14:17:51 +0000

    Added all improvements to Arduno code

--- follow/follow.ino
+++ follow/follow.ino
@@ -5,7 +5,7 @@ int j;                // Looping variable
 int inPin[3];         // Input pins coming from the ADC
 int roundPin[3];       // Rounded pin values
 int outPin = 13;      // Output serial pin for motor control
-int timeDelay = 500;    // Delay between issuing a signal and reading, milliseconds
+int timeDelay = 50;    // Delay between issuing a signal and reading, milliseconds
 
 int proxPin = 11;     // Digital input pin used for the proximity sensor
 int proxVal;          // Value from the input of the proximity pin
@@ -24,7 +24,22 @@ int compare = 50;
 int inputVoltage[2];  // Inputs to the Sabretooth
 
 //int lookup[][5] = {{0,0,0,0,0},{0,0,1,-3,1},{0,0,3,-5,1},{0,1,1,-2,-4},{0,2,2,4,2},{0,2,3,-9,2},{0,2,4,0,1},{0,3,2,0,4},{0,3,4,-5,-4},{0,4,1,-8,2},{0,4,2,-4,2},{0,4,3,0,-7},{1,0,1,-9,-7},{1,0,4,2,2},{2,0,4,-6,-9},{2,3,0,2,2},{3,0,4,-8,8},{3,4,0,3,3},{4,1,0,-7,-7},{4,2,0,-9,-8},{4,3,0,2,4}};
-int lookup[][5] = {{0,0,0,0,0},{0,0,1,5,-5},{0,0,3,5,-5},{0,1,1,5,-5},{0,2,2,6,-6},{0,2,3,7,-7},{0,2,4,8,-8},{0,3,2,5,-5},{0,3,4,8,-8},{0,4,1,-8,8},{0,4,2,-7,7},{0,4,3,6,-6},{1,0,1,9,7},{1,0,3,6,4},{1,0,4,6,2},{2,0,2,6,4},{2,0,4,6,5},{2,3,0,5,6},{3,0,1,9,8},{3,0,4,9,6},{3,2,0,6,8},{3,4,0,6,9},{4,0,1,8,8},{4,0,2,8,9},{4,0,3,9,6},{4,1,0,8,8},{4,2,0,8,9},{4,3,0,6,9}};
+int lookup[][5] = {{0,0,0, 0, 0},{0,0,1, 5,-5},{0,0,3, 5,-5},{0,0,3,5,-5},{0,0,4,5,-5}
+                  ,{0,1,0, 5,-5},{0,1,1, 5,-5},{0,1,2, 5,-5},{0,1,3,5,-5},{0,1,4,5,-5}
+                  ,{0,2,0, 6,-6},{0,2,1, 6,-6},{0,2,2, 6,-6},{0,2,3,7,-7},{0,2,4,8,-8}
+                  ,{0,3,0, 5,-5},{0,3,1, 5,-5},{0,3,2, 5,-5},{0,3,3,8,-8},{0,3,4,8,-8}
+                  ,{0,4,0,-8, 8},{0,4,1,-8, 8},{0,4,2,-7, 7},{0,4,3,6,-6},{0,4,4,6,-6}
+                  ,{1,0,0, 9, 7},{1,0,1, 9, 7},{1,0,2, 9, 7},{1,0,3,6, 4},{1,0,4,6, 2}
+                  ,{1,1,0, 9, 7},{1,1,1, 9, 7},{1,1,2, 9, 7},{1,1,3,6, 4},{1,1,4,6, 2}
+                  ,{1,2,0, 9, 7},{1,2,1, 9, 7},{1,2,2, 9, 7},{1,2,3,6, 4},{1,2,4,6, 2}
+                  ,{1,3,0, 9, 7},{1,3,1, 9, 7},{1,3,2, 9, 7},{1,3,3,6, 4},{1,3,4,6, 2}
+                  ,{1,4,0, 9, 7},{1,4,1, 9, 7},{1,4,2, 9, 7},{1,4,3,6, 4},{1,4,4,6, 2}
+                  ,{2,0,0, 6, 4},{2,0,1, 6, 4},{2,0,2, 6, 4},{2,0,3,6, 4},{2,0,4,6, 5}
+                  ,{2,3,0, 5, 6},{2,3,1, 5, 6},{2,3,2, 5, 6},{2,3,3,5, 6},{2,3,4,5, 6}
+                  ,{3,0,0, 9, 8},{3,0,1, 9, 8},{3,0,2, 9, 6},{3,0,3,9, 6},{3,0,4,9, 6}
+                  ,{3,1,0, 6, 8},{3,1,1, 6, 8},{3,2,2, 6, 8},{3,3,3,6, 9},{3,4,4,6, 9}
+                  ,{4,2,0, 8, 8},{4,2,1, 8, 8},{4,2,2, 8, 9},{4,2,3,9, 6},{4,2,4,9, 6}
+                  ,{4,3,0, 8, 8},{4,3,1, 8, 8},{4,3,2, 8, 9},{4,3,3,6, 9},{4,3,4,6, 9}};
 
 
 SoftwareSerial ST = SoftwareSerial(0, 13); // Serial variable to control the motor
@@ -217,28 +232,28 @@ void loop() {
     
     delay(timeDelay);
 
-//    Serial.print("Packet: ");
-//    // Print the input sensor data to be used for the test
-//    for (i=0;i<3;i++)
-//    {
-//      Serial.print(inPin[i]);
-//      Serial.print(",");
-//      inPin[i] = analogRead(i);
-//    }
-//    
-//    // Print the voltages that will be used for the test
-//    Serial.print(inputVoltage[0]);
-//    Serial.print(",");
-//    Serial.print(inputVoltage[1]);
-//    Serial.print(",");
-//    
-//    // Print the output sensor values of the test
-//    Serial.print(inPin[0]);
-//    Serial.print(",");
-//    Serial.print(inPin[1]);
-//    Serial.print(",");
-//    Serial.print(inPin[2]);
-//    Serial.print("\n");
+    Serial.print("Packet: ");
+    // Print the input sensor data to be used for the test
+    for (i=0;i<3;i++)
+    {
+      Serial.print(inPin[i]);
+      Serial.print(",");
+      inPin[i] = analogRead(i);
+    }
+    
+    // Print the voltages that will be used for the test
+    Serial.print(inputVoltage[0]);
+    Serial.print(",");
+    Serial.print(inputVoltage[1]);
+    Serial.print(",");
+    
+    // Print the output sensor values of the test
+    Serial.print(inPin[0]);
+    Serial.print(",");
+    Serial.print(inPin[1]);
+    Serial.print(",");
+    Serial.print(inPin[2]);
+    Serial.print("\n");
   }
 }
 
